ae3d398384e3f978d51bd1b07ed6a44d84e6c46b
Lavanya Jose <lavanyaj@cs.stanford.edu>
2016-08-15 05:47:40 +0000

    flow sizes, avg works

--- flow-size-distribution.lua
+++ flow-size-distribution.lua
@@ -1,10 +1,10 @@
 local ffi = require("ffi")
 
 ffi.cdef [[
-struct {
+typedef struct {
     double cdf_;
     double val_;
-} CDFentry;;
+} CDFentry;
 ]]
 
 local fsdMod = {
@@ -18,11 +18,12 @@ local fsdMod = {
    ["INTER_INTEGRAL"] = 2, -- linear and round up
 }
 
-function fsdMod:new (o)
-   o = o or {}
-   setmetatable(o, self)
-   self.__index = self
-   return o
+fsdMod.__index = fsdMod
+
+function fsdMod.create ()
+   local fsd = {}
+   setmetatable(fsd, fsdMod)
+   return fsd
 end
 
 function fsdMod:loadCDF(filename)
@@ -32,13 +33,15 @@ function fsdMod:loadCDF(filename)
    end         
    self.table_ = ffi.new("CDFentry[?]", numLines)
    local lineNum = 0
-   for line in io.lines(filename) do
-      local val, index, cdf = line:match("(%d+) (%d+) (%d+)")
+   for line in io.lines(filename) do      
+      local val, index, cdf = line:match("(.+)%s+(.+)%s+(.+)")
+      print(line)
+      print(val .. ", " .. index .. ", " .. cdf)
       assert(val ~= nil)
       assert(index ~= nil)
       assert(cdf ~= nil)
-      table_[lineNum].val = val
-      table_[lineNum].cdf = cdf
+      self.table_[lineNum].val_ = tonumber(val)
+      self.table_[lineNum].cdf_ = tonumber(cdf)
       lineNum = lineNum + 1
    end
 
@@ -48,14 +51,14 @@ end
 
 function fsdMod:avg()
    local avg = 0
-   for i = 0, self.numEntry-1 do
+   for i = 0, self.numEntry_-1 do
       local value = 0
       local prob = 0
       if i == 0 then
-	 value = self.table_[0].val/2
+	 value = self.table_[0].val_/2
 	 prob = self.table_[0].cdf_
       else
-	 value = (self.table_[i-1].val + self.table_[i].val)/2
+	 value = (self.table_[i-1].val_ + self.table_[i].val_)/2
 	 prob = self.table_[i].cdf_ - self.table_[i-1].cdf_	 
       end
       avg = avg + value * prob
@@ -94,4 +97,8 @@ function fsdMod:lookup(u)
    return lo
 end
 
+return fsdMod
 
+--local f = fsdMod.create()
+--f:loadCDF("DCTCP_CDF")
+--print(f:avg())
